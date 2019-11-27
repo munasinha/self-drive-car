e2a831bf50238c75e40748a99a14e1bc3fe7c6ca
Navod Linux <navod199736@gmail.com>
2019-11-27 11:30:53 +0530

    foword fun

--- ai.py
+++ ai.py
@@ -17,10 +17,15 @@ from torch.autograd import Variable
 # Creating the architecture of neural netwotk
 
 class Netwotk(nn.Module):
-    
+
     def __init__(self, input_size, nb_action):
         super(Netwotk, self).__init__()
         self.input_size = input_size
         self.fc1 = nn.Linear(input_size , 30) # create a full connection bitween input layer and hidden layer
         self.fc2 = nn.Linear(30 , nb_action)  # create a full connection between hidden layer and the out put layer
+
+    def forward(self , state):
+        x = F.relu(self.fc1(state)) #activate the hidden neurons
+        q_values = self.fc2(x)      # output neurons for the q-value
+        return q_values
 
