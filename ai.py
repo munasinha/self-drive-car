e889912c14ffbe1535f3e18b27e3ce683748d2b5
Navod Linux <navod199736@gmail.com>
2019-11-27 11:28:47 +0530

    import libs

---
+++ ai.py
@@ -0,0 +1,17 @@
+# 
+# ******************************** AI self driving car ****************************************
+# 
+
+# import libs
+
+import numpy as np 
+import random
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F 
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
+
+
